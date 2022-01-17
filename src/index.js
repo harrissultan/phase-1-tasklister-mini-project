c98ab6b062588f1e7b919ed1bb71859e37b9dde1
Harris Sultan <harris_sultan@outlook.com>
2022-01-16 21:25:56 -0500

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const inputField = document.getElementById("new-task-description");
+  
+  const form1 = document.getElementById("create-task-form");
+  console.log(form1);
+  form1.addEventListener("submit" , function(pD) { 
+    pD.preventDefault();
+    console.log(addtask(inputField.value)); 
+  });
+}); 
+function addtask(task){
+  let mainlist = document.createElement('li');
+  mainlist.textContent= `${task}`;
+  document.getElementById("tasks").appendChild(mainlist);
+}
